5d168e4485717006f5a0b14baeb9f9b2069c5433
Kanokpron10 <kanokpron@kkumail.com>
2019-06-24 18:18:44 +0700

    ADD : สร้างฟังก์ชัน และฟังก์ชันทดสอบ CalculateDurationStartTimeToEndTime

--- calculate_test.go
+++ calculate_test.go
@@ -48,3 +48,75 @@ func Test_GetFullDate_Year_1996_Month_2_Day_5_Should_Get_Monday_5_February_1996(
 		t.Errorf("Expected %s but go %s",expectedResult,actualResult)
 	}
 }
+
+func Test_CalculateDurationStartTimeToEndTime_StartYear_1997_StartMonth_10_StartDay_16_And_EndYear_2019_EndMonth_6_EndDay_10_Should_Get_683164800_Seconds(t *testing.T){
+	start := YearMonthDay{
+		year : 1997,
+		month : 10,
+		day : 16,
+	}
+	end := YearMonthDay{
+		year : 2019,
+		month : 6,
+		day : 10,
+	}
+	expectedResult := Duration{
+		seconds:683164800,
+		minutes:11386080,
+		hours:189768,
+	}
+
+	actualResult := CalculateDurationStartTimeToEndTime(start,end)
+
+	if expectedResult != actualResult {
+		t.Errorf("Expected %v but go %v",expectedResult,actualResult)
+	}
+}
+
+func Test_CalculateDurationStartTimeToEndTime_StartYear_1997_StartMonth_10_StartDay_27_And_EndYear_2019_EndMonth_6_EndDay_10_Should_Get_682214400_Seconds(t *testing.T){
+	start := YearMonthDay{
+		year : 1997,
+		month : 10,
+		day : 27,
+	}
+	end := YearMonthDay{
+		year : 2019,
+		month : 6,
+		day : 10,
+	}
+	expectedResult := Duration{
+		seconds:682214400,
+		minutes:11370240,
+		hours:189504,
+	}
+
+	actualResult := CalculateDurationStartTimeToEndTime(start,end)
+
+	if expectedResult != actualResult {
+		t.Errorf("Expected %v but go %v",expectedResult,actualResult)
+	}
+}
+
+func Test_CalculateDurationStartTimeToEndTime_StartYear_1996_StartMonth_2_StartDay_5_And_EndYear_2019_EndMonth_6_EndDay_10_Should_Get_736646400_Seconds(t *testing.T){
+	start := YearMonthDay{
+		year : 1996,
+		month : 2,
+		day : 5,
+	}
+	end := YearMonthDay{
+		year : 2019,
+		month : 6,
+		day : 10,
+	}
+	expectedResult := Duration{
+		seconds:736646400,
+		minutes:12277440,
+		hours:204624,
+	}
+
+	actualResult := CalculateDurationStartTimeToEndTime(start,end)
+
+	if expectedResult != actualResult {
+		t.Errorf("Expected %v but go %v",expectedResult,actualResult)
+	}
+}
