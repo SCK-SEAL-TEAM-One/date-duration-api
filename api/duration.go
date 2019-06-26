e19af27296ca50b2f555060e5c095ddbe0e0c519
uracha <uracha_p@kkumail.com>
2019-06-26 15:21:34 +0700

    EDIT : แก้ API ให้มีการเรียกใช้ฟังก์ชัน

--- api/duration.go
+++ api/duration.go
@@ -1,66 +1,45 @@
 package api
 
 import (
+	"../date"
 	"encoding/json"
 	"fmt"
 	"io/ioutil"
 	"net/http"
 )
 
-func CalculateDateDuration(w http.ResponseWriter, r *http.Request) {
-
-GetRequestDateFromRequestBody(r)
-	fmt.Fprintf(w,`{
-"start_full_date":"Thursday, 16 October 1997",
-"end_full_date":"Monday, 10 June 2019",
-"duration":{
-"seconds":683164800,
-"minutes":11386080,
-"hours":189768,
-"days":7907,
-"weeks":{"weeks":1129,"days":4},
-"months":{"months":259,"days":25}
-}
-}`)
-}
-
-func GetRequestDateFromRequestBody(r *http.Request){
-	body,_:=ioutil.ReadAll(r.Body)
-	var requestDate RequestDate
-	_ = json.Unmarshal(body,&requestDate)
-	fmt.Printf("%+v",requestDate)
-}
-
 type RequestDate struct {
-	StartDate YearMonthDay `json:"start_date"`
-	EndDate YearMonthDay `json:"end_date"`
-}
-
-type YearMonthDay struct {
-	Year  int `json:"year"`
-	Month int `json:"month"`
-	Day   int `json:"day"`
+	StartDate date.YearMonthDay `json:"start_date"`
+	EndDate   date.YearMonthDay `json:"end_date"`
 }
 
 type ResponseBody struct {
-	StartFullDate string `json:"start_full_date"`
-	EndFullDate string `json:"end_full_date"`
-	Duration Duration `json:"duration"`
-}
-type Duration struct {
-	Seconds int `json:"seconds"`
-	Minutes int `json:"minutes"`
-	Hours int `json:"hours"`
-	Days int `json:"days"`
-	Weeks Weeks `json:"weeks"`
-	Months Months `json:"months"`
-}
-type Weeks struct {
-	Weeks int `json:"weeks"`
-	Days int `json:"days"`
+	StartFullDate string        `json:"start_full_date"`
+	EndFullDate   string        `json:"end_full_date"`
+	Duration      date.Duration `json:"duration"`
 }
 
-type Months struct {
-	Months int `json:"months"`
-	Days int `json:"days"`
+func CalculateDateDuration(w http.ResponseWriter, r *http.Request) {
+	requestDate := GetRequestDateFromRequestBody(r)
+	duration := ResponseBody{
+		StartFullDate: requestDate.StartDate.GetFullDate(),
+		EndFullDate:   requestDate.EndDate.GetFullDate(),
+		Duration: date.Duration{
+			Seconds: date.CalculateDurationStartTimeToEndTime(requestDate.StartDate,requestDate.EndDate).Seconds,
+			Minutes: date.CalculateDurationStartTimeToEndTime(requestDate.StartDate,requestDate.EndDate).Minutes,
+			Hours:   date.CalculateDurationStartTimeToEndTime(requestDate.StartDate,requestDate.EndDate).Hours,
+			Days:    date.CalculateDurationStartTimeToEndTime(requestDate.StartDate,requestDate.EndDate).GetDays(),
+			Months:  date.CalculateMonths(requestDate.StartDate,requestDate.EndDate),
+		},
+	}
+
+	durationJSON,_ := json.Marshal(duration)
+	fmt.Fprintf(w, "%+v",string(durationJSON))
+}
+
+func GetRequestDateFromRequestBody(r *http.Request) RequestDate {
+	body, _ := ioutil.ReadAll(r.Body)
+	var requestDate RequestDate
+	_ = json.Unmarshal(body, &requestDate)
+	return requestDate
 }
