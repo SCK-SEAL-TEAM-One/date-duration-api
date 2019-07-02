5db7a4a70f48df041cf3648782924919a901de5c
endkarn <end_karn@hotmail.com>
2019-07-02 15:47:28 +0700

    EDIT : จัด format ของโค้ดโดย go fmt

--- cmd/handler/calculation_test.go
+++ cmd/handler/calculation_test.go
@@ -11,39 +11,39 @@ import (
 	"testing"
 )
 
-func Test_CalculateDuration_Between_Mo_Birthday_And_10_June_2019(t *testing.T)  {
+func Test_CalculateDuration_Between_Mo_Birthday_And_10_June_2019(t *testing.T) {
 	expectedResult := date.Duration{
-		Seconds:683164800,
-		Minutes :11386080,
-		Hours:189768,
-		Days :7907,
-		Weeks :date.Weeks{
-			TotalWeeks :1129,
-			DaysOfWeek:4,
+		Seconds: 683164800,
+		Minutes: 11386080,
+		Hours:   189768,
+		Days:    7907,
+		Weeks: date.Weeks{
+			TotalWeeks: 1129,
+			DaysOfWeek: 4,
 		},
-		Months :date.Months{
-			TotalMonths:259,
-			DaysOfMonth:25,
+		Months: date.Months{
+			TotalMonths: 259,
+			DaysOfMonth: 25,
 		},
-		StartFullDate :"Thursday, 16 October 1997",
-		EndFullDate :"Monday, 10 June 2019",
+		StartFullDate: "Thursday, 16 October 1997",
+		EndFullDate:   "Monday, 10 June 2019",
 	}
 	w := httptest.NewRecorder()
-	request := httptest.NewRequest("POST","http://localhost:8080/date/calculate",strings.NewReader(`{"start_date":{"year":1997,"month":10,"day":16},"end_date":{"year":2019,"month":6,"day":10}}`))
-	CalculateDuration(w,request)
+	request := httptest.NewRequest("POST", "http://localhost:8080/date/calculate", strings.NewReader(`{"start_date":{"year":1997,"month":10,"day":16},"end_date":{"year":2019,"month":6,"day":10}}`))
+	CalculateDuration(w, request)
 	response := w.Result()
-	if actualHttpStatus := response.StatusCode; http.StatusOK != actualHttpStatus{
-		t.Fatalf("Exception HTTP Status %d",actualHttpStatus)
+	if actualHttpStatus := response.StatusCode; http.StatusOK != actualHttpStatus {
+		t.Fatalf("Exception HTTP Status %d", actualHttpStatus)
 	}
 	body, err := ioutil.ReadAll(response.Body)
-	if err != nil{
-		t.Fatalf("Exeption %v",err.Error())
+	if err != nil {
+		t.Fatalf("Exeption %v", err.Error())
 	}
 
 	var actualResult date.Duration
 	err = json.NewDecoder(bytes.NewReader(body)).Decode(&actualResult)
-	if err != nil{
-		t.Fatalf("Exeption %v",err.Error())
+	if err != nil {
+		t.Fatalf("Exeption %v", err.Error())
 	}
 
 	if expectedResult != actualResult {
@@ -51,24 +51,24 @@ func Test_CalculateDuration_Between_Mo_Birthday_And_10_June_2019(t *testing.T)  
 	}
 }
 
-func Test_DecodeDate_By_StartYear_1997_StartMonth_10_StartDay_16_And_EndYear_2019_EndMonth_6_EndDay_10_Should_Get_Dates(t *testing.T)  {
-	request := httptest.NewRequest("POST","http://localhost:8080/date/calculate",strings.NewReader(`{"start_date":{"year":1997,"month":10,"day":16},"end_date":{"year":2019,"month":6,"day":10}}`))
+func Test_DecodeDate_By_StartYear_1997_StartMonth_10_StartDay_16_And_EndYear_2019_EndMonth_6_EndDay_10_Should_Get_Dates(t *testing.T) {
+	request := httptest.NewRequest("POST", "http://localhost:8080/date/calculate", strings.NewReader(`{"start_date":{"year":1997,"month":10,"day":16},"end_date":{"year":2019,"month":6,"day":10}}`))
 	expectedResult := Dates{
-		StartDate:YearMonthDay{
-			Year:1997,
-			Month:10,
-			Day:16,
+		StartDate: YearMonthDay{
+			Year:  1997,
+			Month: 10,
+			Day:   16,
 		},
-		EndDate:YearMonthDay{
-			Year:2019,
-			Month:6,
-			Day:10,
+		EndDate: YearMonthDay{
+			Year:  2019,
+			Month: 6,
+			Day:   10,
 		},
 	}
 
-	actualResult,err := decodeDate(request);
-	if err != nil{
-		t.Fatalf("Exeption %v",err.Error())
+	actualResult, err := decodeDate(request)
+	if err != nil {
+		t.Fatalf("Exeption %v", err.Error())
 	}
 
 	if expectedResult != actualResult {
@@ -76,24 +76,24 @@ func Test_DecodeDate_By_StartYear_1997_StartMonth_10_StartDay_16_And_EndYear_201
 	}
 }
 
-func Test_DecodeDate_By_StartYear_1997_StartMonth_10_StartDay_27_And_EndYear_2019_EndMonth_6_EndDay_10_Should_Get_Dates(t *testing.T)  {
-	request := httptest.NewRequest("POST","http://localhost:8080/date/calculate",strings.NewReader(`{"start_date":{"year":1997,"month":10,"day":27},"end_date":{"year":2019,"month":6,"day":10}}`))
+func Test_DecodeDate_By_StartYear_1997_StartMonth_10_StartDay_27_And_EndYear_2019_EndMonth_6_EndDay_10_Should_Get_Dates(t *testing.T) {
+	request := httptest.NewRequest("POST", "http://localhost:8080/date/calculate", strings.NewReader(`{"start_date":{"year":1997,"month":10,"day":27},"end_date":{"year":2019,"month":6,"day":10}}`))
 	expectedResult := Dates{
-		StartDate:YearMonthDay{
-			Year:1997,
-			Month:10,
-			Day:27,
+		StartDate: YearMonthDay{
+			Year:  1997,
+			Month: 10,
+			Day:   27,
 		},
-		EndDate:YearMonthDay{
-			Year:2019,
-			Month:6,
-			Day:10,
+		EndDate: YearMonthDay{
+			Year:  2019,
+			Month: 6,
+			Day:   10,
 		},
 	}
 
-	actualResult,err := decodeDate(request);
-	if err != nil{
-		t.Fatalf("Exeption %v",err.Error())
+	actualResult, err := decodeDate(request)
+	if err != nil {
+		t.Fatalf("Exeption %v", err.Error())
 	}
 
 	if expectedResult != actualResult {
@@ -101,24 +101,24 @@ func Test_DecodeDate_By_StartYear_1997_StartMonth_10_StartDay_27_And_EndYear_201
 	}
 }
 
-func Test_DecodeDate_By_StartYear_1996_StartMonth_2_StartDay_5_And_EndYear_2019_EndMonth_6_EndDay_10_Should_Get_Dates(t *testing.T)  {
-	request := httptest.NewRequest("POST","http://localhost:8080/date/calculate",strings.NewReader(`{"start_date":{"year":1996,"month":2,"day":5},"end_date":{"year":2019,"month":6,"day":10}}`))
+func Test_DecodeDate_By_StartYear_1996_StartMonth_2_StartDay_5_And_EndYear_2019_EndMonth_6_EndDay_10_Should_Get_Dates(t *testing.T) {
+	request := httptest.NewRequest("POST", "http://localhost:8080/date/calculate", strings.NewReader(`{"start_date":{"year":1996,"month":2,"day":5},"end_date":{"year":2019,"month":6,"day":10}}`))
 	expectedResult := Dates{
-		StartDate:YearMonthDay{
-			Year:1996,
-			Month:2,
-			Day:5,
+		StartDate: YearMonthDay{
+			Year:  1996,
+			Month: 2,
+			Day:   5,
 		},
-		EndDate:YearMonthDay{
-			Year:2019,
-			Month:6,
-			Day:10,
+		EndDate: YearMonthDay{
+			Year:  2019,
+			Month: 6,
+			Day:   10,
 		},
 	}
 
-	actualResult,err := decodeDate(request);
-	if err != nil{
-		t.Fatalf("Exeption %v",err.Error())
+	actualResult, err := decodeDate(request)
+	if err != nil {
+		t.Fatalf("Exeption %v", err.Error())
 	}
 
 	if expectedResult != actualResult {
